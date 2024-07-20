fb25e0150e7c28289e85f27ebeafbc39e5b912bb
renzo-11 <81481017+renzo-11@users.noreply.github.com>
2024-07-20 15:42:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-  return '<h1>Hello from Flask & Docker</h2>'
+  return '<h1>Chayanne Meza Iriarte</h2>'
 
 if __name__=="__main__":
   app.run(debug=True)
